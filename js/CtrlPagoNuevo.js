4a5e6e424065a4844dbc6870be0395740a4be552
QuintaDelLago <quintadellago2021@gmail.com>
2021-12-14 20:40:33 -0600

    CtrlPagoNuevo

--- js/CtrlPagoNuevo.js
+++ js/CtrlPagoNuevo.js
@@ -13,45 +13,6 @@ import {
   tieneRol
 } from "./seguridad.js";
 
-"use strict";
-var formageneral = document.getElementById("forma"),
-    spnpaquete = document.getElementById("paquete"),
-    numinvitados = document.getElementById("numerodeinvitados"),
-    montoapagar = document.getElementById("montoapagar"),
-    montopagado = document.getElementById("montopagado"),
-    faltante = document.getElementById("faltante");
-
-formageneral["calcularapagar"].addEventListener("click", pago, false);
-formageneral["calcularfaltante"].addEventListener("click", faltapagar, false);
-formageneral["guardar"].addEventListener("click", guarda, false);
-function pago() {
-    try {
-        valida(isNaN(numinvitados.value) || numinvitados.value<=0,"Ingrese el número de invitados");
-        var pago = spnpaquete.value * numinvitados.valueAsNumber;
-        montoapagar.value = pago;
-    } catch (error) {
-        alert(error.message)  
-    }    
-}
-
-function faltapagar() {
-    try {
-        valida(isNaN(montoapagar.value) || montoapagar.value<=0,"Primero calcule el monto a pagar");
-        valida(isNaN(montopagado.value) || montopagado.value<=0,"Ingrese el monto pagado correctamente");
-        var falta = montoapagar.value - montopagado.valueAsNumber;
-        valida(falta<0, "El pago es mayor al monto acordado, cheque registro");
-        valida(falta==0, "Pago saldado, ya puede eliminar el registro");
-        faltante.value = falta; 
-    } catch (error) {      
-        alert(error.message)  
-    }    
-}
-
-function valida(cond, mensaje){
-    if(cond){
-        throw new Error(mensaje);
-    }
-}
 
 const daoPagos = getFirestore().collection("Pagos");
 /** @type {HTMLFormElement} */
@@ -64,7 +25,7 @@ getAuth().onAuthStateChanged(protege, muestraError);
 async function protege(usuario) {
   if (tieneRol(usuario,
     ["Administrador"])) {
-    forma.addEventListener("click", guarda);
+    formageneral["guardar"].addEventListener("click", guarda, false);
   }
 }
 
@@ -99,6 +60,47 @@ async function guarda(evt) {
     muestraPagos();
   } catch (e) {
     muestraError(e);
+  }
+}
+
+
+"use strict";
+var formageneral = document.getElementById("forma"),
+    spnpaquete = document.getElementById("paquete"),
+    numinvitados = document.getElementById("numerodeinvitados"),
+    montoapagar = document.getElementById("montoapagar"),
+    montopagado = document.getElementById("montopagado"),
+    faltante = document.getElementById("faltante");
+
+formageneral["calcularapagar"].addEventListener("click", pago, false);
+formageneral["calcularfaltante"].addEventListener("click", faltapagar, false);
+
+function pago() {
+  try {
+      valida(isNaN(numinvitados.value) || numinvitados.value<=0,"Ingrese el número de invitados");
+      var pago = spnpaquete.value * numinvitados.valueAsNumber;
+      montoapagar.value = pago;
+  } catch (error) {
+      alert(error.message)  
+  }    
+}
+
+function faltapagar() {
+  try {
+      valida(isNaN(montoapagar.value) || montoapagar.value<=0,"Primero calcule el monto a pagar");
+      valida(isNaN(montopagado.value) || montopagado.value<=0,"Ingrese el monto pagado correctamente");
+      var falta = montoapagar.value - montopagado.valueAsNumber;
+      valida(falta<0, "El pago es mayor al monto acordado, cheque registro");
+      valida(falta==0, "Pago saldado, ya puede eliminar el registro");
+      faltante.value = falta; 
+  } catch (error) {      
+      alert(error.message)  
+  }    
+}
+
+function valida(cond, mensaje){
+  if(cond){
+      throw new Error(mensaje);
   }
 }
 
