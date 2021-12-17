7559075d44943e4bee072b16eb5f988569bdac48
QuintaDelLago <quintadellago2021@gmail.com>
2021-12-17 10:17:34 -0600

    Pago

--- js/CtrlPagoNuevo.js
+++ js/CtrlPagoNuevo.js
@@ -69,18 +69,18 @@ async function guarda(evt) {
 
 async function pago() {
   try {
-    valida(isNaN(forma.numerodeinvitados.value) || forma.numerodeinvitados.value<=0,"Ingrese el número de invitados");
+    valida((isNaN(forma.numerodeinvitados.value) || forma.numerodeinvitados.valueAsNumber<=0),"Ingrese el número de invitados");
     var pago = forma.precioppersona.value * forma.numerodeinvitados.value;
     forma.mapagar.value = pago;
   } catch (error) {
     alert(error.message)  
   }    
-  }
+}
   
 async function faltapagar() {
   try {
-    valida(isNaN(forma.mapagar.value) || forma.mapagar.value<=0,"Primero calcule el monto a pagar");
-    valida(isNaN(forma.mpagado.value) || forma.mpagado.value<=0,"Ingrese el monto pagado correctamente");
+    valida((isNaN(forma.mapagar.value) || forma.mapagar.valueAsNumber<=0),"Primero calcule el monto a pagar");
+    valida((isNaN(forma.mpagado.value) || forma.mpagado.valueAsNumber<=0),"Ingrese el monto pagado correctamente");
     var falta = forma.mapagar.value - forma.mpagado.value;
     valida(falta<0, "El pago es mayor al monto acordado, cheque registro");
     valida(falta==0, "Pago saldado, ya puede eliminar el registro");
@@ -88,10 +88,10 @@ async function faltapagar() {
   } catch (error) {      
     alert(error.message)  
   }    
-  }
+}
   
 async function valida(cond, mensaje){
-  if(!cond){
+  if(cond){
     throw new Error(mensaje);
   }
-  }
+}
