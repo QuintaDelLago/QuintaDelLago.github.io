7a660e4dab46910aef4be81ca3cabc67f374d54a
QuintaDelLago <quintadellago2021@gmail.com>
2021-12-17 14:48:32 -0600

    trabajador

--- js/CtrlTrabajador.js
+++ js/CtrlTrabajador.js
@@ -93,7 +93,9 @@ async function elimina() {
     if (confirm("Confirmar la " +
       "eliminación")) {
       await daoTrabajadores.doc(id).delete();
-  //    await eliminaStorage(id);
+      const formData = new FormData(forma);
+      const avatar = formData.get("avatar");
+      await eliminaStorage(avatar);
       muestraTrabajadores();
     }
   } catch (e) {
